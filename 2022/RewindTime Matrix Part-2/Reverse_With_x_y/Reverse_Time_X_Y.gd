096d94ff6e75e1bfdb5c4161a40a18493c7d77ef
Madhavprasath <Madhavprasath088@gmail.com>
2022-01-10 13:06:42 +0530

    Rewind_Time_with_Matrices

---
+++ 2022/RewindTime Matrix Part-2/Reverse_With_x_y/Reverse_Time_X_Y.gd
@@ -0,0 +1,43 @@
+extends Matrix
+class_name Rewind
+export(int)var maximum_seconds=20
+var time_gap=1
+var maximum_times=60*20
+var time_matrix=[]
+var already_pressed=false
+export(int)var segments=4
+var  total_times=0
+var index=0
+"""Stores the value of position in one row,Rotation in other 
+	 Facing value ,Destroyed or Not ting This is Based on matrixes for 
+	
+"""
+func _ready() -> void:
+	set_physics_process(false)
+	time_matrix=initalize_total_value_matrix(2,2,[])
+	for i in range(1200):
+		time_matrix[0][0].append(i)
+func updateposition(pos):
+	var positions_arr=time_matrix[0][0]
+	if(len(positions_arr)>maximum_times):
+		positions_arr.pop_back() 
+	positions_arr.insert(0,pos)
+
+func _physics_process(delta: float) -> void:
+	var a
+	a=time_matrix[0][0].pop_front()
+	if(a!=null):
+		print(a)
+func _unhandled_input(event: InputEvent) -> void:
+	if(total_times<segments):
+		if(event.is_action_pressed("ui_accept")&&!already_pressed):
+			set_physics_process(true)
+			already_pressed=true
+			$Timer.start(maximum_seconds/segments)
+			total_times+=1
+
+
+
+func _on_Timer_timeout() -> void:
+	set_physics_process(false)
+	already_pressed=false
